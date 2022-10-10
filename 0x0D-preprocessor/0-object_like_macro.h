a9a975e2ab76218899eb961912caa03869f9f596
Wildflower1713 <megeniang@gmail.com>
2022-10-10 05:33:40 -0700

    Task 0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,5 @@
 #include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
@@ -13,5 +14,4 @@ int main(void)
     s = 98 + SIZE;
     printf("%d\n", s);
     return (0);
-
 }
