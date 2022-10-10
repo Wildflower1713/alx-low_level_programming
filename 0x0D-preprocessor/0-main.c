6b6511b799dd763ce6af403ce8131afa90b588bb
Wildflower1713 <megeniang@gmail.com>
2022-10-10 05:49:34 -0700

    Task 2

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,1 +1,12 @@
-0x0c. macro, structures
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
